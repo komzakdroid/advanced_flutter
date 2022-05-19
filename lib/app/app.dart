a3ab5d36166439e3e06540201a96f63a8b2a69b6
Komiljon Zokirov <humotechgroup@gmail.com>
2022-05-19 14:29:11 +0500

    Created:  routes_manager.dart  resources package  and all views package

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:advanced_flutter/presentation/resources/theme_manager.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
@@ -15,6 +16,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: getApplicationTheme(),
+    );
   }
 }
---
+++ lib/presentation/resources/styles_manager.dart
@@ -0,0 +1,45 @@
+import 'package:advanced_flutter/presentation/resources/font_manager.dart';
+import 'package:flutter/material.dart';
+
+TextStyle _getTextStyle(
+    double fontSize, String fontFamily, FontWeight fontWeight, Color color) {
+  return TextStyle(
+      fontSize: fontSize,
+      fontFamily: fontFamily,
+      fontWeight: fontWeight,
+      color: color);
+}
+
+/// regular text style **/
+TextStyle getRegularStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConsts.fontFamily, FontWeightManager.regular, color);
+}
+
+/// light text style **/
+TextStyle getLightStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConsts.fontFamily, FontWeightManager.light, color);
+}
+
+/// bold text style
+TextStyle getBoldStyle({double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConsts.fontFamily, FontWeightManager.bold, color);
+}
+
+/// semiBold text style
+TextStyle getSemiBoldStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConsts.fontFamily, FontWeightManager.semiBold, color);
+}
+
+/// medium text style
+TextStyle getMediumStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConsts.fontFamily, FontWeightManager.medium, color);
+}
